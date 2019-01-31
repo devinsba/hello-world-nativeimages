239724201be03f44db865f8787e63fbeebdabefe
Brian Devins-Suresh <badevins@gmail.com>
2019-01-31 12:48:45 -0500

    Add sparkjava

---
+++ sparkjava/src/main/java/com/briandevins/nativeimages/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.briandevins.nativeimages;
+
+import static spark.Spark.get;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
