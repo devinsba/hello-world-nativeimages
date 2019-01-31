5284a8fc6c568370d9619b3b8086ef436f20b93c
Brian Devins-Suresh <badevins@gmail.com>
2019-01-31 09:41:28 -0500

    Undertow works

---
+++ undertow/src/main/java/com/briandevins/nativeimages/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.briandevins.nativeimages;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class HelloWorld {
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+    }
+}
