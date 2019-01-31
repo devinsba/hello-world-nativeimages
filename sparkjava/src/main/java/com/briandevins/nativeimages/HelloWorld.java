b0c31daa0a922d1171220b5406cd48b297094a40
Brian Devins-Suresh <badevins@gmail.com>
2019-01-31 12:56:34 -0500

    Make things consistent

--- sparkjava/src/main/java/com/briandevins/nativeimages/HelloWorld.java
+++ sparkjava/src/main/java/com/briandevins/nativeimages/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.briandevins.nativeimages;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        port(8080);
+        get("/", (req, res) -> "Hello World");
     }
 }
